da96e9d41de8b307eb72c523666ce39db3ad3662
Tim Rothaemel <timrothamel@gmail.com>
2025-11-16 20:32:46 +0100

    Initialize Supabase client and add script reference in index.html

---
+++ src/js/supabse/supabaseClient.js
@@ -0,0 +1,12 @@
+console.log("Supabase client initialized");
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  "https://rptmuyzeiupavmuscrqs.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwdG11eXplaXVwYXZtdXNjcnFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0OTAzNzcsImV4cCI6MjA3NjA2NjM3N30.8j6FajKlzwmj2RJ_EKV8ElCKVwTXoAChWEx2wuFGt0s"
+);
+
+export default supabase;
+
+console.log("Supabase client exported");
